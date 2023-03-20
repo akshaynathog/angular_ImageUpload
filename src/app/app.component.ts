dba9e4ac070425f0995f4d4f9001611474d04cc7
Akshaynath <ogakshaynath@gmail.com>
2023-03-21 00:07:57 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,27 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'ytubeImageUpload';
+  question : string;
+  image:File;
+
+  constructor(private http:HttpClient){}
+
+  onTitleChanged(event:any){
+    this.question=event.target.value;
+  }
+
+  onImageChanged(event:any){
+    this.image=event.target.files[0];
+  }
+
+  newBook(){
+    const uploadData = new FormData();
+    uploadData.append('question', this.question);
+    uploadData.append('image', this.image, this.image.name);
+    this.http.post('http://127.0.0.1:8000/api/questions/',uploadData).subscribe(
+      data=> console.log(data),
+      error=> console.log(error),
+    );
+
+  }
 }
